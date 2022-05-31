0d9396fe609a933be43d5c6dde23d197834cb6ba
Ioan Zaharia <zaharia.ioan@gmail.com>
2022-05-31 11:53:19 +0200

    mongodb connection, contact form connection api

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,10 +1,12 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
     if (
       !email ||
-      !email.include('@') ||
+      !email.includes('@') ||
       !name ||
       name.trim() === '' ||
       !message ||
@@ -14,11 +16,34 @@ function handler(req, res) {
       return;
     }
 
-    //Store in database
     const newMessage = { email, name, message };
 
-    console.log(newMessage);
-    res.status(201).json({ message: 'Message send!' });
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://raw2002:soxaOFaVBk5IWsWj@cluster0.7ueiz.mongodb.net/blog?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      // res.status(500).json({message: error.message})
+      res.status(500).json({ message: 'Could not connect to database.' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed!' });
+      return;
+    }
+
+    client.close();
+
+    res.status(201).json({ message: 'Message send!', message2: newMessage });
   }
 }
 
